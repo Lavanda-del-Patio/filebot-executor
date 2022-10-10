03123906b11c0b1354f6291e7a929c2ea6769d96
Luis Carlos <luisca_jl@hotmail.com>
2022-10-10 19:20:10 +0200

    Allow all cors

--- src/main/java/es/lavanda/filebot/executor/config/CorsConfiguration.java
+++ src/main/java/es/lavanda/filebot/executor/config/CorsConfiguration.java
@@ -10,8 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
-                .exposedHeaders("*").allowedOrigins("*");
+        registry.addMapping("/**");
     }
 }
