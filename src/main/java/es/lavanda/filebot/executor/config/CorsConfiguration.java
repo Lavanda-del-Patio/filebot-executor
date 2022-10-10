781a009ee44f2dbd37c37eb8a2ef1277903d768b
Luis Carlos <luisca_jl@hotmail.com>
2022-10-10 20:31:42 +0200

    Commented corsConfiguration

--- src/main/java/es/lavanda/filebot/executor/config/CorsConfiguration.java
+++ src/main/java/es/lavanda/filebot/executor/config/CorsConfiguration.java
@@ -1,18 +1,21 @@
-package es.lavanda.filebot.executor.config;
+// package es.lavanda.filebot.executor.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-@EnableWebMvc
-public class CorsConfiguration implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowCredentials(true)
-                .allowedMethods("*").allowedHeaders("*")
-                .exposedHeaders("*").allowedOrigins("http://localhost:4200", "https://lavandadelpatio.es",
-                        "https://pre.lavandadelpatio.es", "https://api.lavandadelpatio.es");
-    }
-}
+// @Configuration
+// @EnableWebMvc
+// public class CorsConfiguration implements WebMvcConfigurer {
+//     @Override
+//     public void addCorsMappings(CorsRegistry registry) {
+//         registry.addMapping("/**")
+//                 .allowCredentials(true)
+//                 .allowedMethods("*")
+//                 .allowedHeaders("*")
+//                 .exposedHeaders("*")
+//                 .allowedOrigins("http://localhost:4200", "https://lavandadelpatio.es",
+//                         "https://pre.lavandadelpatio.es", "https://api.lavandadelpatio.es");
+//     }
+// }
